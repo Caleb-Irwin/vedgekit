6029fe4cc403e1f83bacbcd57a8c8a733451b31f
Caleb Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2023-12-16 00:08:51 -0600

    test 4

--- src/routes/api/home/1.jpeg/+server.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const prerender = true;
-
-export const GET: RequestHandler = async ({ fetch }) => {
-	return fetch(
-		'/api/home/https%3A%2F%2Fimg.shopofficeonline.com%2Fvenxia%2FAttachment%2F02-OP-Top-Ad-E-EN.png.jpg'
-	);
-};
